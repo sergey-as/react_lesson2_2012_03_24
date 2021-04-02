af46974761a1803ca885a623c4830860e8ec262d
sergey-as <sergey_as@ukr.net>
2021-04-03 02:38:19 +0300

    1234

--- src/App.js
+++ src/App.js
@@ -8,24 +8,24 @@ import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const counter = 0;
+    let counter = 0;
+
+    const clickHandler = () => {
+        counter++
+        console.log(counter);
+
+        return 123;
+    }
+
+    return (
+        <div className="App">
+            <h2>
+                {counter}
+            </h2>
+            <button onClick={clickHandler}>inc</button>
+        </div>
+    );
 }
 
 export default App;
