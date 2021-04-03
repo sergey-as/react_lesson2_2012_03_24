5bb5aebb1b864977a9dc4dc94f9bd35d061b9969
sergey-as <sergey_as@ukr.net>
2021-04-03 12:32:11 +0300

    12345

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 // state lecture
@@ -6,20 +6,41 @@ import './App.css';
 // show how to update state in fn and class
 // lists?
 
+const Header = ({counter}) => {
+    console.log('child rerender');
+    return (
+        <h2>
+            {counter}
+        </h2>
+    )
+}
 
 function App() {
     // const counter = 0;
-    let counter = 0;
+    // let counter = 0;
+    const [counter, setCounter] = useState(0);
+    const [counter2, setCounter2] = useState(0);
+    const [counter3, setCounter3] = useState(0);
+    //....
 
     const clickHandler = () => {
-        counter++
-        console.log(counter);
+        // counter++
+        // console.log(counter);
 
-        return 123;
+        // setCounter(counter+1);
+        const newCounter = counter + 1;
+        setCounter(newCounter);
+        // console.log(newCounter);
+        // console.log(counter);
+
+        // return 123;
     }
+
+    console.log('parent rerender');
 
     return (
         <div className="App">
+            <Header counter={counter}/>
             <h2>
                 {counter}
             </h2>
